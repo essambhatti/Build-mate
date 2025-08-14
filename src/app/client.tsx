f4e3d32189bc67e50bab3280f3c28ae26faaa55b
Essam Bhatti <essambhatti@gmail.com>
2025-08-15 02:44:49 +0500

    trpc setup

---
+++ src/app/client.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query"
+
+const Client = () => {
+    const trpc = useTRPC()
+    const {data } = useSuspenseQuery(trpc.hello.queryOptions({text : "Hello"}))
+
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  )
+}
+
+export default Client
